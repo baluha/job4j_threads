a7d2213728665c3b0c769c4ae0168a98e4fc0db7
baluha <romanbalushka@gmail.com>
2022-11-24 19:37:24 +0300

    3. Денежные переводы AccountStorage

--- src/test/java/ru/job4j/cash/AccountStorageTest.java
+++ src/test/java/ru/job4j/cash/AccountStorageTest.java
@@ -3,8 +3,7 @@ package ru.job4j.cash;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AccountStorageTest {
-
+public class AccountStorageTest {
     @Test
     void whenAdd() {
         var storage = new AccountStorage();
@@ -44,14 +43,5 @@ class AccountStorageTest {
                 .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
         assertThat(firstAccount.amount()).isEqualTo(0);
         assertThat(secondAccount.amount()).isEqualTo(200);
-    }
-
-    @Test
-    void whenException() throws IllegalStateException {
-        var storage = new AccountStorage();
-        storage.add(new Account(1, 100));
-        storage.add(new Account(2, 100));
-        storage.transfer(1, 2, 100);
-        var firstAccount = storage.getById(5);
     }
 }
