793736a29b74dbefe6ca0b03c48b9513faa82f36
baluha <romanbalushka@gmail.com>
2022-12-03 12:14:13 +0300

    0. Управление нитью через wait.

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -13,20 +13,37 @@ public class CountBarrier {
 
     public void count() {
         synchronized (monitor) {
+            while (count <= total) {
                 count++;
+                System.out.println(count);
                 notifyAll();
+            }
         }
+
     }
 
     public void await() {
         synchronized (monitor) {
-            while (count < total) {
+            while (count <= total) {
                 try {
                     wait();
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
             }
+            System.out.println("Counting is over!");
         }
+    }
+
+    public static void main(String[] args) {
+        CountBarrier cb = new CountBarrier(9);
+        Thread thread1 = new Thread(
+                cb::count
+        );
+        Thread thread2 = new Thread(
+                cb::await
+        );
+        thread1.start();
+        thread2.start();
     }
 }
