730c14134c237c177c8cebb2c0e2e126302f61ff
Baluha <xatowar@gmail.com>
2022-12-06 07:56:34 +0300

    2. Обеспечить остановку потребителя

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -4,33 +4,33 @@ import ru.job4j.synch.SimpleBlockingQueue;
 
 public class ParallelSearch {
 
-    public static void main(String[] args) {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(10);
-        final Thread consumer = new Thread(
-                () -> {
-                    while (!Thread.currentThread().isInterrupted()) {
-                        try {
-                            System.out.println(queue.poll());
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    System.out.println(queue.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
                 }
-        );
+            }
+        });
         consumer.start();
-        new Thread(
-                () -> {
-                    for (int index = 0; index != 3; index++) {
-                        try {
-                            queue.offer(index);
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                    consumer.interrupt();
+        Thread producer = new Thread(() -> {
+            for (int index = 0; index != 3; index++) {
+                try {
+                    queue.offer(index);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
                 }
-
-        ).start();
+            }
+        });
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
     }
 }
