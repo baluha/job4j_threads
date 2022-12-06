a334d26ea627014dd83c75306f68b0e5c8157aee
Baluha <xatowar@gmail.com>
2022-12-06 13:58:16 +0300

    0. CAS - операции

--- src/main/java/ru/job4j/CASCount.java
+++ src/main/java/ru/job4j/CASCount.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+    private final AtomicReference<Integer> count = new AtomicReference<>();
 
     public void increment() {
         int v;
