5123675a6b7dcb2ee8f0c339168b7f40cd2bd3c7
Baluha <xatowar@gmail.com>
2022-12-06 08:14:35 +0300

    3. Junit тест для блокирующей очереди.

--- src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
@@ -1,11 +1,13 @@
 package ru.job4j.synch;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class SimpleBlockingQueueTest {
@@ -18,26 +20,26 @@ class SimpleBlockingQueueTest {
         AtomicInteger value = new AtomicInteger();
         Thread thread1 = new Thread(
                 () -> {
-                        for (int i = 0; i < 10; i++) {
-                            try {
-                                queue.offer(i);
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                            }
+                    for (int i = 0; i < 10; i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
                 }
         );
         Thread thread2 = new Thread(
                 () -> {
-                        for (int i = 0; i < 10; i++) {
-                            try {
-                                int val = queue.poll();
-                                value.set(val);
-                                lst.add(val);
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                            }
+                    for (int i = 0; i < 10; i++) {
+                        try {
+                            int val = queue.poll();
+                            value.set(val);
+                            lst.add(val);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
                         }
+                    }
                 }
         );
         thread1.start();
@@ -50,5 +52,40 @@ class SimpleBlockingQueueTest {
         }
         assertThat(value.get()).isEqualTo(9);
         assertThat(lst).isEqualTo(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+    }
+
+    @Test
+    void whenFetchAllThanGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().isInterrupted();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().isInterrupted();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        Assertions.assertEquals(buffer, List.of(0, 1, 2, 3, 4));
     }
 }
